184330c5bc3e46265181f7d8f2173085325185e8
tom13131 <bike13@juno.com>
2025-12-12 17:23:46 -0500

    squah
    squash

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,142 @@
+package frc.robot;
+
+import java.util.EnumSet;
+
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.AlignToReefFieldRelativePose3D;
+import frc.robot.commands.AlignToReefTagRelativeArcade2D;
+import frc.robot.commands.AlignToReefTargetRelativeTransform3D;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.utils.CommandSchedulerLog;
+import frc.robot.utils.CommandSchedulerLog.CommandStageSelector;
+import frc.robot.utils.CommandSchedulerLog.LogsSelector;
+import frc.robot.vision.VisionContainer;
+import frc.robot.vision.VisionContainer.VisionSelector;
+
+/**
+ * {@link Robot} gets you here and this starts the fan-out to everything and everywhere else
+ */
+public class RobotContainer {
+    static {
+        System.out.println("Loading: " + java.lang.invoke.MethodHandles.lookup().lookupClass().getCanonicalName());
+    }
+
+    private static Drivetrain drivetrain = new Drivetrain();
+    private static VisionContainer visionContainer;
+
+    private static final CommandXboxController driverController = new CommandXboxController(0);
+
+    public RobotContainer() {
+
+        configDataLog();
+
+        // FIXME select command logging options
+        new CommandSchedulerLog(EnumSet.allOf(CommandStageSelector.class),
+                EnumSet.of(LogsSelector.useConsole, LogsSelector.useDataLog));
+
+        // FIXME use VisionSelector to choose which of the vision systems to use
+        var visionSelector = VisionSelector.usePhotonVision;
+        DriverStation.reportWarning("vision selection " + visionSelector, false);
+        visionContainer = new VisionContainer(visionSelector);
+
+        configAButton();
+        configBButton();
+        configXButton();
+    }
+
+    /**
+     * activate 3d pose driving to target using field coordinates
+     */
+    private static void configAButton() {
+        Trigger aButton = driverController.a();
+        if (getVisionContainer().vision()) {
+            aButton.whileTrue(new AlignToReefFieldRelativePose3D(true, getVisionContainer(), getDrivetrain()));
+        } else {
+            aButton.whileTrue(Commands.print("No Vision").repeatedly());
+        }
+    }
+
+    /**
+     * activate 2-d pose driving to target (yaw and pitch)
+     */
+    private static void configBButton() {
+        Trigger bButton = driverController.b();
+        if (getVisionContainer().vision()) {
+            bButton.whileTrue(new AlignToReefTagRelativeArcade2D(getVisionContainer(), getDrivetrain()));
+        } else {
+            bButton.whileTrue(Commands.print("No Vision").repeatedly());
+        }
+    }
+
+    /**
+     * activate 3d pose driving to target using camera coordinates
+     */
+    private static void configXButton() {
+        Trigger xButton = driverController.x();
+        if (getVisionContainer().vision()) {
+            xButton.whileTrue(new AlignToReefTargetRelativeTransform3D(true, visionContainer, getDrivetrain()));
+        } else {
+            xButton.whileTrue(Commands.print("No Vision").repeatedly());
+        }
+    }
+
+    /**
+     * Select what to log with WPILib datalog
+     * <p>
+     * Caution that {@link CommandSchedulerLog} assumes its logging is turned on and not suppressed
+     * by interactions with {@link #configDataLog}
+     * <p>
+     * Logging to internal drive can cause insufficient space eventually.
+     * Plug in a FAT32 USB drive for better space usage.
+     */
+    public static void configDataLog() {
+        // FIXME select data log options
+        DataLogManager.start(); // default is log all NT
+
+        // // partial example (commented out) on how to select what to log
+        // // logging of the "Constants." are illustrative of what could be done and are not in this project
+        // DataLogManager.logNetworkTables(false);
+        // var dataLog = DataLogManager.getLog();
+        // DriverStation.startDataLog(dataLog, true);
+        // var networkTableInstance = NetworkTableInstance.getDefault();
+        // networkTableInstance.startEntryDataLog(dataLog, "/FMSInfo", "NT:/FMSInfo");
+        // networkTableInstance.startEntryDataLog(dataLog, "/" + Constants.NETWORK_TABLE_NAME, "NT:/" + Constants.NETWORK_TABLE_NAME);
+        // networkTableInstance.startEntryDataLog(dataLog, "/" + Constants.ADVANTAGE_SCOPE_TABLE_NAME, "NT:/" + Constants.ADVANTAGE_SCOPE_TABLE_NAME);
+        // networkTableInstance.startEntryDataLog(dataLog, "/" + Constants.Camera.CAMERA_1_BOT_POSE, "NT:/" + Constants.Camera.CAMERA_1_BOT_POSE);
+        // networkTableInstance.startEntryDataLog(dataLog, "/SmartDashboard", "NT:/SmartDashboard");
+        // networkTableInstance.startEntryDataLog(dataLog, "/Shuffleboard", "NT:/Shuffleboard");
+        // networkTableInstance.startEntryDataLog(dataLog, "/LiveWindow", "NT:/LiveWindow");
+        // networkTableInstance.startConnectionDataLog(dataLog, "NTConnection");
+    }
+
+    /**
+     * Class getters are useful if commands that reference them are organized in different files
+     * than where the classes are instantiated
+     */
+
+    /**
+     * @return an existing instance of VisionContainer
+     */
+    public static VisionContainer getVisionContainer() {
+        return visionContainer;
+    }
+
+    /**
+     * @return an existing instance of Drivetrain
+     */
+    public static Drivetrain getDrivetrain() {
+        return drivetrain;
+    }
+
+    /**
+     * {@link Robot#robotPeriodic()} will call this method before the command scheduler and this
+     * method runs all the rest of the methods that must be run before the command scheduler
+     */
+    public static void runBeforeCommands() {
+        getVisionContainer().update();
+    }
+}
---
+++ src/main/java/frc/robot/vision/VisionContainer.java
@@ -0,0 +1,399 @@
+package frc.robot.vision;
+
+import static edu.wpi.first.units.Units.Milliseconds;
+import static edu.wpi.first.units.Units.Seconds;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotController;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+import frc.robot.commands.AlignToReefFieldRelativePose3D;
+import frc.robot.commands.AlignToReefTagRelativeArcade2D;
+import frc.robot.commands.AlignToReefTargetRelativeTransform3D;
+import static frc.robot.utils.Network.getMyIPAddress;
+import static frc.robot.Constants.Vision.Camera;
+
+/**
+ * This class is a wrapper for the three vision classes {@link ControllerVision}, {@link
+ * PhotonVision}, and {@link LimelightVision}
+ * <p>
+ * ControllerVision (roboRIO) returns team specified robot pose based on the tag ID that is closest
+ * to the robot (this needs work; that might not always be appropriate).
+ * <p>
+ * PhotonVision returns its best pose (but not the very best pose estimator that includes odometry
+ * and gyro that teams should add).
+ * <p>
+ * LimelightVision returns team specified filtered selection from MegaTag or MegaTag2.
+ * (This is a reasonable default but could use more tuning. See {@link LimelightVision#update()})
+ * 
+ * <p>
+ * After selecting and configuring the desired vision system, use the robot pose as in example
+ * commands {@link AlignToReefFieldRelativePose3D}, {@link AlignToReefTargetRelativeTransform3D}
+ * and {@link AlignToReefTagRelativeArcade2D}
+ * 
+ * <p>
+ * Getters are available for each of the three vision systems. This allows use of other low level
+ * methods that may be available within the systems' classes. That also helps reveal the following
+ * getters which are duplicative with the RobotPose class data so there is no reason to use them.
+ * <p>
+ * Each vision system extends CameraBase which provides for
+ * 
+ * <pre>
+ * <code>
+ *public abstract void update();
+ *public abstract boolean isFresh();
+ *public abstract int getTagID();
+ *public abstract Pose3d getPose3d();
+ *public abstract Pose2d getPose2d();
+ *public abstract double getTX();
+ *public abstract double getTY();
+ *public abstract Transform3d getCameraToTarget();
+ * </code>
+ * </pre>
+ * <p>
+ * Usage of these methods requires that after "update()" the "isFresh()" is checked before using
+ * any of the getters.
+ * <p>
+ * Since all of these data are available in the RobotPose there isn't a reason to use these methods
+ * in this example project. Other projects may use them.
+ */
+public class VisionContainer {
+    static {
+        System.out.println("Loading: " + java.lang.invoke.MethodHandles.lookup().lookupClass().getCanonicalName());
+
+        System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME); // OpenCV required for all Vision Processes
+
+        try { // likely needed for 3-D pose usage
+            Class.forName(frc.robot.vision.AprilTagsLocations.class.getCanonicalName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public enum VisionSelector {
+        useControllerVision, usePhotonVision, useLimelightVision
+    }
+
+    private VisionSelector visionSelector;
+
+    private ControllerVision controllerVision = null; // only for Controller (roboRIO)
+    private PhotonVision photonVision = null; // only for PV
+    private LimelightVision limelightVision = null; // only for LL
+    private AtomicReference<Optional<RobotPose>> robotPose = new AtomicReference<Optional<RobotPose>>(Optional.empty());
+    private Transform3d robotToCamera;
+    private boolean vision = false; // initially not activated
+
+    public VisionContainer(VisionSelector visionSelector) {
+        this.visionSelector = visionSelector;
+
+        switch (visionSelector) {
+            case useControllerVision:
+                /*
+                 * Select the camera model and resolution from the list of cameras
+                 * that have calibration data.
+                 * 
+                 * Select if the Pose3D calculation is to be performed.
+                 * 
+                 * Enter the 3-D translation (location) of your camera wrt the bottom, center of
+                 * the robot.
+                 * 
+                 * Caution: roboRIO v1 has constrained memory and cpu.
+                 * 
+                 * For v1 use camera resolution about 320x240 maybe a little higher.
+                 * Experiment with your team usage.
+                 * 
+                 * "usePose3D" determines if the time consuming calculation for 3-D pose is
+                 * performed. If this setting is "false", then only the pitch and yaw of the robot
+                 * wrt the AprilTag is available and can they can be used for what LimeLight
+                 * describes as "servoing" mode. Note that the Pose3d is still "available" but it's
+                 * value is zero (origin; not useful). roboRIO v1 can do Pose3D but little time is
+                 * left for other processes. v1 memory limits AprilTag bitsCorrected to 1 error bit
+                 * (up to 2 on roboRIO v2)
+                 * 
+                 * Select the camera device id.
+                 * 
+                 * For real roboRIO, if only one camera, it doesn't matter which physical USB port
+                 * is used - both are 0.
+                 * 
+                 * If in Simulation mode on a Windows PC, usually this is correct for camera id:
+                 * If the external USB camera is plugged in at (before) boot-up time,
+                 * then it is cameraDeviceId = 0 after booting.
+                 * If the PC has an internal camera (common with laptops) and the
+                 * external camera is plugged in after boot-up, then the cameraDeviceId = 1.
+                 * 
+                 * Enter the camera location wrt the robot. Example camera mounted facing
+                 * forward (zero radians yaw), half meter forward of center, quarter meter up from
+                 * center, zero meters left of center, pitch pointing up 25 degrees.
+                 * (camera pointing up is "-""; down is "+"").
+                 */
+                // FIXME need camera ID, camera option, and camera mount
+                var cameraDeviceId = 0;
+                if (ControllerVision.isAvailable(cameraDeviceId)) {
+                    boolean usePose3D = true;
+                    var robotToCameraCV = new Transform3d(new Translation3d(0.5, 0.0, 0.25),
+                            new Rotation3d(0, Units.degreesToRadians(-25.), 0));
+                    robotToCamera = robotToCameraCV;
+                    controllerVision = new ControllerVision(cameraDeviceId,
+                            Camera.ARDUCAM_320x240, usePose3D, robotToCameraCV);
+
+                    // vision runs as a "background" thread to minimize interference with the other
+                    // robot actions. This may increase latency. Priority could be experimented with.
+                    Thread acquireControllerCamera = new Thread(controllerVision);
+                    acquireControllerCamera.setName("roboRIOCameraPose");
+                    acquireControllerCamera.setPriority(acquireControllerCamera.getPriority() - 1);
+                    acquireControllerCamera.setDaemon(true);
+                    acquireControllerCamera.start();
+                    vision = true;
+                } else {
+                    DriverStation.reportWarning("No ControllerVision process", false);
+                    System.out.println("Make sure camera is plugged in" +
+                            (Robot.isSimulation() ? " and number is correct (try the other one)" : ""));
+
+                    controllerVision = null;
+                }
+
+                break;
+
+            case usePhotonVision:
+                /*
+                 * Select the camera name
+                 * 
+                 * Enter the camera location wrt the robot. Example camera mounted facing
+                 * forward (zero radians yaw), half meter forward of center, quarter meter up from center,
+                 * zero meters left of center, pitch pointing up 25 degrees. (camera pointing up is "-""; down is "+"")
+                 */
+                // FIXME select the camera name set in PV dashboard and camera mount
+                var cameraName = "Microsoft_LifeCam_HD-3000"; // "Arducam_OV9281_USB_Camera"
+                var pitch = Units.degreesToRadians(-25.);
+                var robotToCameraPV = new Transform3d(new Translation3d(0.5, 0.0, 0.25),
+                        new Rotation3d(0, pitch, 0));
+                robotToCamera = robotToCameraPV;
+
+                // PV started connecting when the NT server started but still it's a race to make
+                // a connection before this instantiation and check
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                photonVision = new PhotonVision(cameraName, robotToCameraPV);
+
+                final var retryDelay = Seconds.of(1);
+                final var retryLimit = 20;
+
+                checkConnection: {
+                    for (int i = 1; i <= retryLimit; i++) {
+                        if (photonVision.camera.isConnected()) {
+                            vision = true;
+                            break checkConnection;
+                        }
+                        System.out.println("attempt " + i + " of " + retryLimit + " to attach to PhotonVision named "
+                                + cameraName);
+                        try {
+                            Thread.sleep((long) retryDelay.in(Milliseconds));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    photonVision.camera.getAllUnreadResults(); // force PV to issue no camera by that name but there are other PV cameras
+
+                    if (Robot.isSimulation()) {
+                        System.out.println(
+                                "Enter in the PV Settings Team Number/NetworkTables Server Address the IP address of this simulation device on the interface to the PhotonVision device");
+                        try {
+                            System.out.println(
+                                    "If PV running on the same device as this simulation, then use any of the IP addresses assign to that device such as 127.0.0.1, localhost, or "
+                                            +
+                                            InetAddress.getLocalHost().getHostAddress());
+                        } catch (UnknownHostException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(getMyIPAddress());
+                    } else // it's real roboRIO
+                    {
+                        System.out.println("Make sure the team number " + RobotController.getTeamNumber()
+                                + " is entered to attach to NetworkTables");
+                    }
+
+                    DriverStation.reportWarning("No PhotonVision connection", false);
+
+                    photonVision = null; // put it back to null since constructor didn't really make a viable camera
+                }
+
+                break;
+
+            case useLimelightVision:
+                /*
+                 * Caution: Limelight pitch setup has the opposite sign of PhotonVision and ControllerVision
+                 * LL positive pitch is pointing up
+                 * Snap Robot to Floor is only for MegaTag; MegaTag2 always snaps to floor.
+                 * Further than about 1.1m away from tag, MegaTag tends to have a lot of jitter especially in
+                 * the z (height) axis.
+                 * 
+                 * Add additional Limelight setups as indicated below
+                 */
+
+                // FIXME Select the limelight name that is set in the Limelight box through its dashboard
+                var limelightName = "limelight";
+                if (LimelightVision.isAvailable(limelightName)) {
+                    limelightVision = new LimelightVision(limelightName, RobotContainer.getDrivetrain());
+                    var c = LimelightHelpers.getCameraPose3d_RobotSpace(limelightName); // retrieve camera position in LL
+                    var robotToCameraLL = new Transform3d(c.getTranslation(),
+                            new Rotation3d(c.getRotation().getX(), -c.getRotation().getY(), c.getRotation().getZ())); // - pitch to match CV & PV
+                    robotToCamera = robotToCameraLL;
+                    vision = true;
+                    // put additional LL setup as needed
+                    // LimelightHelpers.setStreamMode_PiPSecondary(limelightName); // if there is a driver camera, where to put the image - pick your favorite
+                } else {
+                    DriverStation.reportWarning("No LimelightVision connection", false);
+
+                    limelightVision = null;
+                }
+
+                break;
+
+            default:
+                DriverStation.reportWarning("Invalid vision system option selected in VisionContainer constructor, got: "
+                        + visionSelector.toString(),
+                        true);
+                break;
+        }
+    }
+
+    /**
+     * 
+     * @return if there is a Vision System active
+     */
+    public boolean vision() {
+        return vision;
+    }
+
+    /**
+     * periodically executed to update vision information
+     */
+    public void update() {
+        if (!vision()) {
+            return;
+        }
+
+        switch (visionSelector) {
+            case useControllerVision:
+                controllerVision.update();
+                if (controllerVision.isFresh()) {
+                    robotPose.set(Optional.of(new RobotPose(
+                            controllerVision.getTagID(),
+                            controllerVision.getTX(), controllerVision.getTY(),
+                            controllerVision.getPose3d(),
+                            controllerVision.getCameraToTarget())));
+                } else {
+                    robotPose.set(Optional.empty());
+                }
+
+                break;
+
+            case usePhotonVision:
+                photonVision.update();
+                if (photonVision.isFresh()) {
+                    robotPose.set(Optional.of(new RobotPose(
+                            photonVision.getTagID(),
+                            photonVision.getTX(), photonVision.getTY(),
+                            photonVision.getPose3d(),
+                            photonVision.getCameraToTarget())));
+                    // System.out.println("PV " + photonVision.getTagID() + ", " + photonVision.getTX() + ", " + photonVision.getTY() + ", " + photonVision.getPose3d());
+                } else {
+                    robotPose.set(Optional.empty());
+                }
+
+                break;
+
+            case useLimelightVision:
+                limelightVision.update();
+                if (limelightVision.isFresh()) {
+                    robotPose.set(Optional.of(new RobotPose(
+                            limelightVision.getTagID(),
+                            limelightVision.getTX(), limelightVision.getTY(),
+                            limelightVision.getPose3d(),
+                            limelightVision.getCameraToTarget())));
+                    // System.out.println((limelightVision.getSuggestResetOdometry() ?
+                    // "reset odometry pose " : "addVisionMeasurement pose ") +
+                    // limelightVision.getPose2d() + (limelightVision.isMegaTag2() ? " MegaTag2 pose" : " MegaTag pose"));
+                } else {
+                    robotPose.set(Optional.empty());
+                }
+
+                break;
+
+            default:
+                DriverStation.reportWarning("Invalid vision system option selected in VisionContainer update, got: "
+                        + visionSelector.toString(),
+                        true);
+                break;
+        }
+
+        // getRobotPose().ifPresentOrElse
+        // (
+        // (pose)-> {
+        // var t = getRobotPose().get().cameraToTarget;
+        // System.out.println("tag is forward " + t.getX() + "m, left " + t.getY() + "m, up " + t.getZ() +
+        // "m, roll " + Units.radiansToDegrees(t.getRotation().getX()) +
+        // "deg, pitch " + Units.radiansToDegrees(t.getRotation().getY()) +
+        // "deg, yaw " + Units.radiansToDegrees(t.getRotation().getZ()) + "deg");},
+        // ()-> System.out.print("X")
+        // );
+    }
+
+    /**
+     * Robot pose for whomever wants it
+     * 
+     * @return RobotPose
+     */
+    public Optional<RobotPose> getRobotPose() {
+        return robotPose.get();
+    }
+
+    /**
+     * 
+     * @return location of the camera wrt the robot
+     */
+    public Transform3d getRobotToCamera() {
+        return robotToCamera;
+    }
+
+    /**
+     * Getter for vision system for CameraBase getters
+     * 
+     * @return
+     */
+    public PhotonVision getPhotonVision() {
+        return photonVision;
+    }
+
+    /**
+     * Getter for vision system for CameraBase getters
+     * 
+     * @return
+     */
+    public LimelightVision getLimelightVision() {
+        return limelightVision;
+    }
+
+    /**
+     * Getter for vision system for CameraBase getters
+     * 
+     * @return
+     */
+    public ControllerVision getControllerVision() {
+        return controllerVision;
+    }
+}
