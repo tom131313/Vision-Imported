d33950e386ba3862c6723a416f39f6710fb5b989
tom13131 <bike13@juno.com>
2025-12-15 14:46:59 -0500

    change image handling to single thread code

--- src/main/java/frc/robot/Image.java
+++ src/main/java/frc/robot/Image.java
@@ -7,7 +7,9 @@ import edu.wpi.first.apriltag.AprilTagDetection;
 /**
  * Buffers an image with setImage() method.
  * <p>getImage() method has a thread wait until a new image is set (blocking).
+ * [disabled for this single threaded vision set and get use]
  * <p>Multi-Thread-Safe version (overkill if that isn't needed)
+ * [disabled - no longer thread safe as its use here is single thread]
  */
 public class Image {
    /**
@@ -19,30 +21,30 @@ public class Image {
       private int frameNumber = 0;
       private boolean isFreshImage = false; // indicates a new camera image; no implication if that image is more or less useful than the previous image
 
-      public synchronized void setImage(Mat mat, AprilTagDetection[] detections, double acquisitionTime, int frameNumber)
+      public /*synchronized*/ void setImage(Mat mat, AprilTagDetection[] detections, double acquisitionTime, int frameNumber)
             {
                 mat.copyTo(this.mat);
                 this.detections = detections.clone();
                 this.acquisitionTime = acquisitionTime;
           this.frameNumber = frameNumber;
           this.isFreshImage = true;
-          notify(); // fresh image so tell whoever is waiting for it
+        //   notify(); // fresh image so tell whoever is waiting for it
       }
 
-      public synchronized AprilTagDetection[] getImage(Mat mat,  AcquisitionTime acquisitionTime)
+      public /*synchronized*/ AprilTagDetection[] getImage(Mat mat,  AcquisitionTime acquisitionTime)
       {
-          try
-          {
-              while(!this.isFreshImage) // make sure awakened for the right reason
-              {
-                  wait(0L, 0); // stale image so thread wait for a new image no timeout
-              }
-          } 
-          catch (Exception e)
-          {
-              System.out.println("getImage Exception " + e.toString());
-              throw new RuntimeException(e);
-          }
+        //   try
+        //   {
+        //       while(!this.isFreshImage) // make sure awakened for the right reason
+        //       {
+        //           wait(0L, 0); // stale image so thread wait for a new image no timeout
+        //       }
+        //   } 
+        //   catch (Exception e)
+        //   {
+        //       System.out.println("getImage Exception " + e.toString());
+        //       throw new RuntimeException(e);
+        //   }
           this.isFreshImage = false;
           this.mat.copyTo(mat);
           acquisitionTime.acquisitionTime = this.acquisitionTime;
@@ -50,7 +52,7 @@ public class Image {
           return this.detections;
       }
 
-      public synchronized boolean isFreshImage()
+      public /*synchronized*/ boolean isFreshImage()
       {
           return this.isFreshImage;
       }
